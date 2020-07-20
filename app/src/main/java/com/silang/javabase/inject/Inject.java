ebf743e5f20d3d73964f7f9426b0b318ad47533a
zhongxiaohong <931766850@qq.com>
2020-07-20 17:56:50 +0800

    6241201

---
+++ app/src/main/java/com/silang/javabase/inject/Inject.java
@@ -0,0 +1,14 @@
+package com.silang.javabase.inject;
+
+import androidx.annotation.IdRes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+    @IdRes int id();//TODO 限制 id 必须是资源 id
+}
---
+++ app/src/main/java/com/silang/javabase/inject/InjectTool.java
@@ -0,0 +1,11 @@
+package com.silang.javabase.inject;
+
+import android.app.Activity;
+
+public class InjectTool {
+
+    public static void register(Activity activity){
+        
+
+    }
+}
